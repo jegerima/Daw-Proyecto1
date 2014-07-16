fd8731b159b966bac657f227a1328a3c86995905
Charlie Medina <charliegmv@hotmail.com>
2014-07-16 02:51:35 -0500

    Peticiones pt1
    Se crea el popup que te indica si quieres mandar peticion a esa ruta

--- js/scriptMain.js
+++ js/scriptMain.js
@@ -587,3 +587,40 @@ function guardarComent(){
     popup.parentNode.removeChild(popup);
 }
 
+function confirmarPeticion(){
+   var popup, form, leyenda, txtComent, hora, btn;
+
+    popup = document.createElement("div");
+    popup.setAttribute("class", "popup");
+    popup.setAttribute("id", "popup");
+    salirPopupId = "popup";
+    window.onkeyup = salirPopUp;
+
+    form = document.createElement("form");
+    form.setAttribute("id", "frmPeticion");
+    form.setAttribute("action", "javascript:mandarPeticion()");
+
+    leyenda = document.createElement("div");
+    leyenda.innerHTML = "¿Seguro que desea enviar una peticion a esta ruta?";
+    form.appendChild(leyenda);
+
+    leyenda = document.createElement("div");
+    leyenda.setAttribute("class", "leyenda");
+    btn = document.createElement("input");
+    btn.setAttribute("type", "submit");
+    btn.setAttribute("class", "botonSubmit");
+    btn.setAttribute("value", "Aceptar");
+    leyenda.appendChild(btn);
+    form.appendChild(leyenda);
+
+    popup.appendChild(form);
+
+    seccion = document.getElementById("map-canvas");
+    seccion.appendChild(popup);
+}
+
+function mandarPeticion(){
+    //Manda peticion al usuario de la ruta seleccionada
+    var popup = document.getElementById("popup");
+    popup.parentNode.removeChild(popup);
+}
